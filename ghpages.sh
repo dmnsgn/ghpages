12c74bec90a09d91a856c78111db4e9ea06128df
mattdesl <dave.des@gmail.com>
2016-03-14 23:43:37 -0400

    handle git ignores and initial branch creation

--- ghpages.sh
+++ ghpages.sh
@@ -1,9 +1,15 @@
 PREVIOUS_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
+  
 if [ -z "$(git status --porcelain)" ]; then 
   # Working directory clean
   echo "Deploying gh-pages..."
-  git checkout -B gh-pages
+  if git show-ref --verify --quiet "refs/heads/gh-pages"; then
+    git checkout gh-pages
+  else
+    echo "Creating branch gh-pages..."
+    git checkout -b gh-pages
+  fi
   git merge master --no-edit
   if [[ $* == *-i* ]]; then
     echo "Modifying .gitignores..."
